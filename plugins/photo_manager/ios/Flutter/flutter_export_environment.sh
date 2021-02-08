5bdc022077a686293821e7dc0c385ae80e5d3bca
sewerganger <wanghan9423@outlook.com>
2021-02-09 01:11:20 +0800

    refector: 移除submodule

--- plugins/photo_manager/ios/Flutter/flutter_export_environment.sh
+++ plugins/photo_manager/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Admin\Desktop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Admin\Desktop\tu\lan_file_more\plugins\photo_manager"
+export "FLUTTER_ROOT=C:\my_software\software\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\wangh\Desktop\APPS\lan_file_more\plugins\photo_manager"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\Admin\Desktop\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\my_software\software\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=0.5.8"
 export "FLUTTER_BUILD_NUMBER=0.5.8"
 export "DART_OBFUSCATION=false"
